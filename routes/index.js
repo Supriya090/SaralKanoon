d4a2b923d904bfef932ef1fc7c41ab415e744088
Supriya Khadka <075bct090.supriya@pcampus.edu.np>
2020-11-07 23:46:16 +0545

    Home page created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Saral Kanoon' });
+  res.render('index', { title: 'Saral Kanoon', subTitle:'Law Made Easy' });
 });
 
 module.exports = router;
