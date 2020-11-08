4a9877c2e0880e97768035f718545f247c8985bc
dee-na7721 <deenasitikhu@gmail.com>
2020-11-08 10:33:59 +0545

    deleted sub-cateories pages and Category.ejs edited

--- routes/index.js
+++ routes/index.js
@@ -11,40 +11,6 @@ router.get('/categories', function(req, res, next){
 });
 
 
-router.get('/rape', function(req, res, next){
-  res.render('rape',{title:'Rape'});
-});
-
-router.get('/bestiality', function(req, res, next){
-  res.render('bestiality',{title:'Bestiality'});
-});
-
-
-router.get('/childSA', function(req, res, next){
-  res.render('childSA',{title:'Child Sexual Abuse'});
-});
-
-router.get('/detainneeSI', function(req, res, next) {
-  res.render('detainneeSI', { title: 'Sexual Intercourse with Detainee' });
-});
-
-router.get('/harrassment', function(req, res, next){
-  res.render('harrassment',{title:'Harrassment'});
-});
-
-
-router.get('/incest', function(req, res, next){
-  res.render('incest',{title:'Incest'});
-});
-
-router.get('/underProtectionSI', function(req, res, next){
-  res.render('underProtectionSI',{title:'Sexual Intercourse with Person in One\'s protection ' });
-});
-
-
-router.get('/unnaturalSI', function(req, res, next){
-  res.render('unnaturalSI',{title:'Un-natural Sexual Intercourse'});
-});
 
 
 module.exports = router;
